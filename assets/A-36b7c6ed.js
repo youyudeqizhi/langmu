29fc29e66019f4918ac2962cfadd46f938ec810f
menglingjiao <15558125545@163.com>
2023-03-28 21:38:38 +0800

    124

---
+++ assets/A-36b7c6ed.js
@@ -0,0 +1,1 @@
+import{d as a,c as s,a as r,o as e,r as t}from"./index-6fd36b92.js";const n={class:""},o=a({__name:"A",setup(a){const o=a=>{};return(a,d)=>{const c=t("van-uploader");return e(),s("div",n,[r(c,{"after-read":o})])}}});export{o as default};
